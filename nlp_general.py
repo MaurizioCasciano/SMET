76aaf3d69bf03583e74af0cd67f374f84c1019e5
Maurizio Casciano <maurizio.casciano@eng.it>
2025-01-09 12:15:07 +0100

    Remove unused/broken pattern requirement

--- nlp_general.py
+++ nlp_general.py
@@ -22,7 +22,6 @@ import pickle
 from nltk.stem.wordnet import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 from sklearn.feature_extraction.text import TfidfVectorizer
-from pattern.en import lexeme, pluralize
 from numpy.linalg import norm
 from numpy import dot
 
